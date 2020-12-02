c9431e32b978d03553bb5260cfa72f556330ad18
Niclas Rosengren <niclas.rosengren@gmail.com>
2020-12-02 18:50:55 +0100

    day01

--- src/day01.rs
+++ src/day01.rs
@@ -18,6 +18,84 @@
 //! Of course, your expense report is much larger.
 //! Find the two entries that sum to 2020; what do you get if you multiply them together?
 
-fn main() {
-    println!("P1");
+use std::cmp::max;
+
+pub fn solve() -> crate::Result<()> {
+    let input = crate::read_input("day01.txt")?;
+    println!("Day01 part1: {}", solve_part1(&input));
+    println!("Day01 part2: {}", solve_part2(&input));
+
+    Ok(())
+}
+fn parse_input(s: &str) -> Vec<i64> {
+    s.trim()
+        .lines()
+        .map(|s| s.trim())
+        .map(|s| s.parse::<i64>())
+        .collect::<Result<Vec<_>, _>>()
+        .expect("Day01: parsing")
+}
+
+fn solve_part1(input: &str) -> i64 {
+    let nums = parse_input(input);
+    let (n1, n2) = nums
+        .iter()
+        .enumerate()
+        .filter_map(|(i, n1)| {
+            nums.iter()
+                .skip(i)
+                .find(|&n2| n2 + n1 == 2020)
+                .map(|n2| (n1, n2))
+        })
+        .next()
+        .expect("Day01: solving");
+
+    n1 * n2
+}
+
+fn solve_part2(input: &str) -> i64 {
+    let nums = parse_input(input);
+    let (n1, n2, n3) = nums
+        .iter()
+        .enumerate()
+        .filter_map(|(i1, n1)| {
+            nums.iter()
+                .skip(i1)
+                .enumerate()
+                .filter_map(|(i2, n2)| {
+                    nums.iter()
+                        .skip(max(i1, i2))
+                        .find(|&n3| n1 + n2 + n3 == 2020)
+                        .map(|n3| (n1, n2, n3))
+                })
+                .next()
+        })
+        .next()
+        .expect("Day01: solving");
+
+    n1 * n2 * n3
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    static EXAMPLE_INPUT: &str = r#"
+1721
+979
+366
+299
+675
+1456"#;
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(solve_part2(EXAMPLE_INPUT.trim()), 241861950);
+    }
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(solve_part1(EXAMPLE_INPUT.trim()), 514579);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::{fs::File, io::Read};
+
+mod day01;
+
+pub type Error = Box<dyn std::error::Error + 'static>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub fn read_input(input_file: &str) -> Result<String> {
+    let mut f = File::open(format!("inputs/{}", input_file))?;
+    let mut buf = String::new();
+    f.read_to_string(&mut buf)?;
+    Ok(buf)
+}
+
+fn main() -> Result<()> {
+    match std::env::args().nth(1).map(|s| s.parse::<usize>()) {
+        Some(Ok(1)) => day01::solve()?,
+        Some(Ok(n)) => println!("Day {} is not implemented yet.", n),
+        Some(Err(err)) => println!("Invalid day argument: {}.", err),
+        None => println!("Day argument must be provided."),
     }
+
+    Ok(())
 }
