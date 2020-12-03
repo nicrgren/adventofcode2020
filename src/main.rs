73ae30739ec67c9841e9c1df58ef7a5b703fc181
Niclas Rosengren <niclas.rosengren@gmail.com>
2020-12-03 18:04:02 +0100

    Solved day03 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{fs::File, io::Read};
 
 mod day01;
 mod day02;
+mod day03;
 
 pub type Error = Box<dyn std::error::Error + 'static>;
 pub type Result<T> = std::result::Result<T, Error>;
@@ -17,6 +18,7 @@ fn main() -> Result<()> {
     match std::env::args().nth(1).map(|s| s.parse::<usize>()) {
         Some(Ok(1)) => day01::solve()?,
         Some(Ok(2)) => day02::solve()?,
+        Some(Ok(3)) => day03::solve()?,
         Some(Ok(n)) => println!("Day {} is not implemented yet.", n),
         Some(Err(err)) => println!("Invalid day argument: {}.", err),
         None => println!("Day argument must be provided."),
