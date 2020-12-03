397337d7e15bba408d6d865e2f80ee4b5a804512
Niclas Rosengren <niclas.rosengren@gmail.com>
2020-12-03 18:13:21 +0100

    Solved day03 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -69,26 +69,66 @@
 //! Starting at the top-left corner of your map and following a slope of right 3 and down 1,
 //! how many trees would you encounter?
 //!
+//!
+//!
+//! --- Part Two ---
+//!
+//! Time to check the rest of the slopes - you need to minimize the probability of
+//! a sudden arboreal stop, after all.
+//!
+//! Determine the number of trees you would encounter if, for each of the following slopes,
+//! you start at the top-left corner and traverse the map all the way to the bottom:
+//!
+//!     Right 1, down 1.
+//!     Right 3, down 1. (This is the slope you already checked.)
+//!     Right 5, down 1.
+//!     Right 7, down 1.
+//!     Right 1, down 2.
+//!
+//! In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively;
+//! multiplied together, these produce the answer 336.
+//!
+//! What do you get if you multiply together the number of trees encountered
+//! on each of the listed slopes?
 
 pub fn solve() -> crate::Result<()> {
     let input = crate::read_input("day03.txt")?;
     println!("Day03 part1: {}", solve_part1(&input));
+    println!("Day03 part1: {}", solve_part2(&input));
 
     Ok(())
 }
-
 /// Returns the number of trees encountered.
 fn solve_part1(input: &str) -> usize {
-    let row_step = 3;
-    let mut tree_count = 0;
-    let mut row_index = 0;
+    solver(input, 3, 1)
+}
 
-    for line in input.trim().lines().map(|s| s.trim()) {
-        let c = line.chars().nth(row_index % line.len()).unwrap();
+/// Returns the number of trees encountered.
+fn solve_part2(input: &str) -> usize {
+    solver(input, 1, 1)
+        * solver(input, 3, 1)
+        * solver(input, 5, 1)
+        * solver(input, 7, 1)
+        * solver(input, 1, 2)
+}
+
+/// Returns the number of trees encountered.
+///
+/// # Arguments
+///
+/// * `input` - The input string
+/// * `x_step` - The steps to move on each line per iteration.
+/// * `y_step` - The number of lines to jump after each iteration.
+fn solver(input: &str, x_step: usize, y_step: usize) -> usize {
+    let mut tree_count = 0;
+    let mut col_index = 0;
+
+    for line in input.trim().lines().map(|s| s.trim()).step_by(y_step) {
+        let c = line.chars().nth(col_index % line.len()).unwrap();
         if c == '#' {
             tree_count += 1;
         }
-        row_index += row_step;
+        col_index += x_step;
     }
 
     tree_count
@@ -121,5 +161,16 @@ mod tests {
     fn part1() {
         let input = crate::read_input("day03.txt").expect("Reading input");
         assert_eq!(solve_part1(&input), 184);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(solve_part2(EXAMPLE_INPUT.trim()), 336);
+    }
+
+    #[test]
+    fn part2() {
+        let input = crate::read_input("day03.txt").expect("Reading input");
+        assert_eq!(solve_part2(&input), 2431272960);
     }
 }
