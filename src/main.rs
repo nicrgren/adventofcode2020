4b2efb469707bd275228a326df38d44e3b57e770
Niclas Rosengren <niclas.rosengren@gmail.com>
2020-12-08 18:22:02 +0100

    Day08 part1

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod day04;
 mod day05;
 mod day06;
 mod day07;
+mod day08;
 
 pub type Error = Box<dyn std::error::Error + 'static>;
 pub type Result<T> = std::result::Result<T, Error>;
@@ -27,6 +28,7 @@ fn main() -> Result<()> {
         day05::solve,
         day06::solve,
         day07::solve,
+        day08::solve,
     ];
 
     match std::env::args().nth(1).map(|s| s.parse::<usize>()) {
