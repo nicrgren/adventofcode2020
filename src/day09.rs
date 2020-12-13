f4639d8cd4f2289b6c4bbaa51a5b2b1f6852073f
Niclas Rosengren <niclas.rosengren@gmail.com>
2020-12-13 20:04:12 +0100

    day9 part2

--- src/day09.rs
+++ src/day09.rs
@@ -67,17 +67,98 @@
 //! first number in the list (after the preamble) which is not the sum of two
 //! of the 25 numbers before it.
 //! What is the first number that does not have this property?
+//!
+//!
+//!
+//! --- Part Two ---
+//! The final step in breaking the XMAS encryption relies on the invalid number
+//! you just found: you must find a contiguous set of at least two numbers in
+//! your list which sum to the invalid number from step 1.
+//!
+//! Again consider the above example:
+//!
+//! ```
+//! 35
+//! 20
+//! 15
+//! 25
+//! 47
+//! 40
+//! 62
+//! 55
+//! 65
+//! 95
+//! 102
+//! 117
+//! 150
+//! 182
+//! 127
+//! 219
+//! 299
+//! 277
+//! 309
+//! 576
+//! ```
+//!
+//! In this list, adding up all of the numbers from 15 through 40 produces
+//! the invalid number from step 1, 127.
+//! (Of course, the contiguous set of numbers in your actual list might be much longer.)
+//!
+//! To find the encryption weakness, add together the smallest and largest number
+//! in this contiguous range; in this example, these are 15 and 47, producing 62.
+//!
+//! What is the encryption weakness in your XMAS-encrypted list of numbers?
+
 pub fn solve() -> crate::Result<()> {
     let input = crate::read_input("day09.txt")?;
-    println!("Day09 part1: {}", part1(25, &input));
+    let p1 = part1(25, &input);
+    println!("Day09 part1: {}", p1);
+    println!("Day09 part2: {}", part2(p1, &input));
     Ok(())
 }
 
+/// Find the first invalid numer according to the XMAS protocol.
 fn part1(preamble_size: usize, s: &str) -> u64 {
     verify(preamble_size, s)
         .err()
         .expect("Wanted error")
         .element
+}
+
+/// Find contiguous set of numbers in `s` that sums to target.
+/// Returns sum of smallest and largets number in this set.
+/// Highly unoptimized.
+fn part2(target: u64, s: &str) -> u64 {
+    let numbers: Vec<u64> = s
+        .trim()
+        .lines()
+        .map(|s| s.trim().parse::<u64>().expect("parsing line"))
+        .collect();
+
+    let mut sum = 0;
+    let mut start = 0;
+    let mut end = 0;
+
+    while sum != target {
+        if target < sum {
+            sum -= numbers[start];
+            start += 1;
+        } else {
+            sum += numbers[end];
+            end += 1;
+        }
+    }
+
+    let min = numbers[start..end]
+        .iter()
+        .min()
+        .expect("min of found range");
+    let max = numbers[start..end]
+        .iter()
+        .max()
+        .expect("max of found range");
+
+    min + max
 }
 
 fn verify(preamble_size: usize, s: &str) -> Result<(), XmasDecodeError> {
@@ -151,5 +232,39 @@ mod tests {
     fn part1() {
         let input = crate::read_input("day09.txt").expect("reading input");
         assert_eq!(530627549, super::part1(25, &input));
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = r#"
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+"#
+        .trim();
+        assert_eq!(62, super::part2(127, &input));
+    }
+
+    #[test]
+    fn part2() {
+        let input = crate::read_input("day09.txt").expect("reading input");
+        assert_eq!(77730285, super::part2(530627549, &input));
     }
 }
